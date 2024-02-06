531cc027f75d8ebbc2a9e738b5d09fe75eb5aa61
cassevven <119555884+cassevven@users.noreply.github.com>
2024-02-05 16:01:12 -0800

    created a duck directory

--- components/grid.php
+++ components/grid.php
@@ -1,34 +1,29 @@
 <div class="main">
-<div class="welcome">
-    <h2>Welcome to Duck Domain</h2>
-    <div class="welcome1"><p>Dive into the world of whimsy and quirkiness, where rubber ducks reign supreme! <br>Whether you're a seasoned rubber duck enthusiast or just starting your duck-filled journey, this is your ultimate sanctuary.</p></div>
-</div>
-<div class="card">
-    <div class="cards"><img src="./assets/images/blue.png" alt="blue duck">
-    <ol>
-        <li><h3>Nick Quack</h3></li>
-        <li><h4>Favorite foods</h4></li>
-        <li><p>1. White bread</p></li>
-        <li><p>2. Popcorn</p></li>
-        <li><p>3. Worms</p></li>
-    </ol>
-</div>
-    <div class="cards"><img src="./assets/images/green.png" alt="green duck">
-     <ol>
-        <li><h3>Pat Quack</h3></li>
-        <li><h4>Favorite foods</h4></li>
-        <li><p>1. Cabbage</p></li>
-        <li><p>2. Lettuce</p></li>
-        <li><p>3. Green beans</p></li>
-    </ol>
-</div>
-    <div class="cards"><img src="./assets/images/header_duck.png" alt="yellow duck">
-      <ol>
-        <li><h3>Zach Quack</h3></li>
-        <li><h4>Favorite foods</h4></li>
-        <li><p>1. Leftover pizza</p></li>
-        <li><p>2. Ramen</p></li>
-        <li><p>3. Classic Lays</p></li>
-    </ol>
-</div>
+    <div class="welcome">
+        <h2>Welcome to Duck Domain</h2>
+        <div class="welcome1">
+            <p>Dive into the world of whimsy and quirkiness, where rubber ducks reign supreme! <br>Whether you're a seasoned rubber duck enthusiast or just starting your duck-filled journey, this is your ultimate sanctuary.</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <?php foreach($ducks as $duck) : ?>
+        <div class="cards">
+            <img src="<?php echo $duck["img_src"]; ?>">
+            <ol>
+                <li><h3><?php echo $duck["name"]; ?></h3></li>
+
+                <?php               
+                $foods_list = explode(",", $duck["favorite_foods"]);
+                ?>
+
+                <ul class="favorite-foods">
+                    <?php foreach($foods_list as $food) : ?>
+                        <li><?php echo $food; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </ol>
+        </div>
+        <?php endforeach; ?>
+    </div>
 </div>
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php include('./config/db.php');
+
+$sql = "SELECT name,favorite_foods,img_src,bio FROM ducks";
+
+$result = mysqli_query($conn, $sql);
+
+$ducks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+mysqli_close($conn);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include('./components/head.php'); ?>
