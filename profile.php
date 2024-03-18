a80c5233deefb625e4c915628b3e48f78bafe4dd
cassevven <119555884+cassevven@users.noreply.github.com>
2024-03-18 15:36:30 -0700

    changes

--- profile.php
+++ profile.php
@@ -1,19 +1,65 @@
+<?php
+$duck_is_live = false;
+
+if (isset($_GET['id'])) {
+    $id = htmlspecialchars($_GET['id']);
+    require('./config/db.php');
+    $sql = "SELECT id, name, favorite_foods, bio, img_src FROM ducks WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    
+    $duck = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+    print_r($duck);
+
+    if (isset($duck["id"])) {
+        $duck_is_live = true;
+    }
+
+    if(isset($_POST('submit'))) {
+        $id = $_POST('id_to_delete');
+    }
+    
+}
+?>
+
+// include db connection
+// build profile
+// 
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include('./components/head.php'); ?>
 <body>
 <?php include('./components/nav.php'); ?>
+<main>
+    <?php if ($duck_is_live) : ?>
     <div class="profile">
         <div class="profile-pic">
-            <img src="./assets/images/green.png" alt="green duck">
+            <img src="<?php echo $duck['img_src'];?>" alt="green duck">
         </div>
+        <?php else : ?>
+            <section class="no-duck">
+                <h1>sorry, no duck</h1>
+        </section>
+        <?php endif ?>
         <div class="profile-info">
-        <h2>Pat Quack</h2>
-        <p><strong>Favorite Foods:</strong> Cabbage, Lettuce, and Green Beans</p>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <h2><?php echo $duck['name'];?></h2>
+        <p><strong>Favorite Foods:</strong><?php echo $duck['favorite_foods'];?></p>
+        <p><?php echo $duck['bio'];?></p>
         </div>
     </div>
 
+    <form action="./index.php" method="POST">
+
+  <input type="hidden" name="id_to_delete" value="<?php echo $duck['id']; ?>">
+
+  <input type="submit" name="delete" value="Delete Duck">
+
+    </form>
+
     <?php include('./components/footer.php'); ?>
+        </main>
 </body>
 </html>
